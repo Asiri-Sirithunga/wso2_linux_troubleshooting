def5244d6eab9a5025f8b8a51139a72d659d952a
Asiri Sirithunga <ravinduasiri31@gmail.com>
2021-08-09 14:46:52 -0700

    Add files via upload

---
+++ wso2training/hash_wso2.py
@@ -0,0 +1,15 @@
+import hashlib
+userInput=str(input())
+salt=bytes("Km5d5ivMy8iexuHcZrsD","ascii")
+itterations=200000
+convertedInput = bytes(userInput,"ascii")
+output = hashlib.pbkdf2_hmac('sha512', convertedInput, salt , itterations)
+print(output.hex())
+import hashlib
+userInput=str(input())
+salt=bytes("Km5d5ivMy8iexuHcZrsD","ascii")
+itterations=200000
+convertedInput = bytes(userInput,"ascii")
+output = hashlib.pbkdf2_hmac('sha512', convertedInput, salt , itterations)
+print(output.hex())
+
